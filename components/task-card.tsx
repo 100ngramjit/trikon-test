79dcd2790e6ab7587efba655692a098ae10fe45d
sangramjit dutta <sangramjitdutta@gmail.com>
2024-08-12 20:28:52 +0530

    feat: add card drag and drop

--- components/task-card.tsx
+++ components/task-card.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useDrag } from "react-dnd";
 import { Task } from "@/models/task";
 import { Clock, Pencil, Trash2 } from "lucide-react";
 
@@ -9,13 +10,25 @@ interface TaskCardProps {
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ task, onEdit, onDelete }) => {
-  const handleSave = (updatedTask: Task) => {
-    onEdit(updatedTask);
-  };
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "TASK",
+    item: { id: task.id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const ref = useRef<HTMLDivElement>(null);
+  drag(ref);
 
   return (
     <>
-      <div>
+      <div
+        ref={ref}
+        className={`bg-white p-4 rounded-lg shadow-md mb-3 ${
+          isDragging ? "opacity-50" : ""
+        }`}
+      >
         <h3 className="text-lg text-gray-600 font-semibold mb-2">
           {task.name}
         </h3>
@@ -36,7 +49,10 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, onEdit, onDelete }) => {
             </span>
             Edit
           </button>
-          <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded text-sm inline-flex gap-2">
+          <button
+            onClick={onDelete}
+            className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded text-sm inline-flex gap-2"
+          >
             <span>
               <Trash2 className="h-5 w-5" />
             </span>
