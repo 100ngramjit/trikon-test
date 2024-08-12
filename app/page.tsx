f32455a46df48d8cf8676749ec5373e8b129c298
sangramjit dutta <sangramjitdutta@gmail.com>
2024-08-12 20:31:55 +0530

    feat: add task section and add task modal to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
 "use client";
 
+import { addTask, deleteTask, editTask, moveTask } from "@/store/taskSlice";
+import { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import AddTaskModal from "@/components/add-task-modal";
+import { createTask } from "@/models/task";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
-import { Plus } from "lucide-react";
+import TaskSection from "@/components/task-section";
 
 export default function Home() {
   const tasks = useAppSelector((state) => state.tasks.tasks);
   const dispatch = useAppDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentSection, setCurrentSection] = useState<
+    "to do" | "doing" | "done"
+  >("to do");
+
+  const moveTaskHandler = (
+    id: string,
+    newStatus: "to do" | "doing" | "done"
+  ) => {
+    dispatch(moveTask({ id, newStatus }));
+  };
 
   const renderSection = (status: "to do" | "doing" | "done") => (
-    <div ref={ref} className="flex-1 bg-gray-100 p-4 rounded-lg shadow-md m-2">
-      <h2 className="text-xl font-bold mb-4 text-gray-700">
-        {status.charAt(0).toUpperCase() + status.slice(1)}
-      </h2>
-      <div>
-        {tasks.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
-      <button className="mt-4 bg-none text-gray-400 font-bold py-2 px-2 rounded inline-flex items-center">
-        <span>
-          <Plus />
-        </span>
-        Add a card
-      </button>
-    </div>
+    <TaskSection
+      status={status}
+      tasks={tasks.filter((task) => task.status === status)}
+      onTaskMove={moveTaskHandler}
+      onTaskEdit={(task) => dispatch(editTask(task))}
+      onTaskDelete={(id) => dispatch(deleteTask(id))}
+      onAddCard={() => {
+        setCurrentSection(status);
+        setIsModalOpen(true);
+      }}
+    />
   );
 
   return (
@@ -35,6 +45,14 @@ export default function Home() {
         {renderSection("doing")}
         {renderSection("done")}
       </div>
+      <AddTaskModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onAdd={(name, description) => {
+          dispatch(addTask(createTask(name, description, currentSection)));
+          setIsModalOpen(false);
+        }}
+      />
     </DndProvider>
   );
 }
